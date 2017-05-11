695ab3129a0a40055521ce04cd7abb89afc4dde7
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2017-05-11 10:20:52 +0200

    Config two api versions in Swagger

--- config/initializers/swagger_ui_engine.rb
+++ config/initializers/swagger_ui_engine.rb
@@ -1,5 +1,10 @@
 SwaggerUiEngine.configure do |config|
-  config.swagger_url = '/doc/swagger.yaml'
+  config.swagger_url = {
+    v1: '/doc/swagger.yaml',
+    v2: '/doc/swagger.yaml'
+  }
+
   config.doc_expansion = 'full'
   config.model_rendering = 'model'
+  config.validator_enabled = true
 end
